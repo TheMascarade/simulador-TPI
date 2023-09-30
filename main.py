d0c41ca5910e63964932649734fad7ed44920a8c
Matias Bella <matias.bella@hotmail.com>
2023-09-30 18:18:17 -0300

    ingreso a cola de listos

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import json
 
 
 def main():
-    cargaTrabajo = json.load(open("json/procesos.json"))
+    cargaTrabajo: list[dict] = json.load(open("json/procesos.json"))
     cargaTrabajo.sort(key=lambda d: d["tiempo_arribo"])
     tiempoEjecucion: int = 0
-    colaListos: list[dict]
+    colaListos: list[dict] = []
     for proceso in cargaTrabajo:
         tiempoEjecucion += proceso["tiempo_irrupcion"]
-    for i in range(0, tiempoEjecucion):
+    for instante in range(0, tiempoEjecucion):
         # Aca se maneja la administracion de colaListos, asignacion de memoria, salida y demas
+        colaListos.extend(buscarProcesosEntrantes(instante, cargaTrabajo))
 
 
+def buscarProcesosEntrantes(instante: int, cargaTrabajo: list[dict]):
+    entrantes: list[dict] = []
+    for proceso in cargaTrabajo:
+        if proceso["tiempo_arribo"] == instante:
+            entrantes.append(proceso)
+        if proceso["tiempo_arribo"] > instante:
+            break
+    return entrantes
 
 
 if __name__ == "__main__":
