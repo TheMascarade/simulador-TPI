751b9a679c241e8567bd24bcbf8efd68f7d50ef7
Matias Bella <matias.bella@hotmail.com>
2023-10-08 16:52:12 -0300

    Esquema general

--- main.py
+++ main.py
@@ -1,27 +1,86 @@
 import json
+from typing import Type, TypeVar
 
+
+class Memoria:
+    def __init__(self) -> None:
+        particiones: list['Particion']
+    
+
+class Particion:
+    def __init__(self, id, tam, dir_comienzo, frag_interna):
+        self.id = id
+        self.tam = tam
+        self.dir_comienzo = dir_comienzo
+        self.frag_interna = frag_interna
+        self.observadores: list = []
+        self.proceso: 'Proceso'
+    def cargar_proceso(self, proceso: 'Proceso'):
+        self.proceso = proceso
+    def get_proceso(self):
+        return self.proceso
+
+
+class SistemaOperativo:
+    def __init__(self, carga_trabajo: list['Proceso']):
+        self.carga_trabajo = carga_trabajo
+        self.tiempo_ejecucion: int
+        self.QUANTUM = 2
+        self.procesador = Procesador()
+        self.memoria = Memoria()
+    def correr(self):
+        self.set_tiempo_ejecucion()
+        i = 0
+           
+    def set_tiempo_ejecucion(self):
+        for proceso in self.carga_trabajo:
+            self.tiempo_ejecucion += proceso.tiempo_irrupcion
+    def actualizar(self, msg:str):
+        ()
+
+
+class Procesador:
+    def __init__(self) -> None:
+        self.observadores: list = []
+        self.proceso: 'Proceso'
+        self.clock = 0
+    def ejecutar(self, proceso: 'Proceso'):
+        self.proceso = proceso
+    def correr(self, tiempo_limite:int):
+        for self.clock in range(0, tiempo_limite):
+            if self.proceso.tiempo_irrupcion == 0:
+                self.notificar("Fin proceso")
+            self.proceso.tiempo_irrupcion -= 1
+    def agregar(self, observador):
+        self.observadores.append(observador)
+    def remover(self, observador):
+        self.observadores.remove(observador)
+    def notificar(self, msg: str):
+        for observador in self.observadores:
+            observador.actualizar(msg)
+
+
+class Proceso:
+    def __init__(self, id: int, tam: int, arribo: int, irrupcion: int) -> None:
+        self.id: int = id
+        self.tam: int = tam
+        self.tiempo_arribo: int = arribo
+        self.tiempo_irrupcion: int = irrupcion
+        
 
 def main():
-    cargaTrabajo: list[dict] = json.load(open("json/procesos.json"))
-    cargaTrabajo.sort(key=lambda d: d["tiempo_arribo"])
-    tiempoEjecucion: int = 0
-    colaListos: list[dict] = []
-    for proceso in cargaTrabajo:
-        tiempoEjecucion += proceso["tiempo_irrupcion"]
-    for instante in range(0, tiempoEjecucion):
-        # Aca se maneja la administracion de colaListos, asignacion de memoria, salida y demas
-        colaListos.extend(buscarProcesosEntrantes(instante, cargaTrabajo))
-
-
-def buscarProcesosEntrantes(instante: int, cargaTrabajo: list[dict]):
-    entrantes: list[dict] = []
-    for proceso in cargaTrabajo:
-        if proceso["tiempo_arribo"] == instante:
-            entrantes.append(proceso)
-        if proceso["tiempo_arribo"] > instante:
-            break
-    return entrantes
-
+    lista_procesos = json.load(open("json/procesos.json"))
+    lista_procesos.sort(key=lambda d: d["tiempo_arribo"])
+    carga_trabajo: list['Proceso'] = []
+    for proc in lista_procesos:
+        proceso = Proceso(
+            proc["id"],            
+            proc["tam"],
+            proc["tiempo_arribo"],
+            proc["tiempo_irrupcion"]
+        )
+        carga_trabajo.append(proceso)
+    sim = SistemaOperativo(carga_trabajo)
 
 if __name__ == "__main__":
     main()
