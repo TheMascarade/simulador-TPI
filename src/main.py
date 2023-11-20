61d61f3574187c4e4646c4688770efb89e3d7f7d
Matias Bella <matias.bella@hotmail.com>
2023-11-19 21:08:25 -0300

    Manejo tiempo de espera e irrupcion

--- src/main.py
+++ src/main.py
@@ -104,6 +104,7 @@ class Simulador:
 
             if self.procesoAEjecutar !=None:
                 self.procesoAEjecutar.irrupcion -= 1
+                self.procesoAEjecutar.retorno += 1
                 self.quantum -= 1
             else:
                 continue
@@ -189,6 +190,10 @@ class Simulador:
                 and self.procesoAEjecutar.estado != Estado.Ejecutando
             ):
                 print("quiere ejecutarse algo q no esta listo")
+            if len(self.procesosEnMemoria) != 0:
+                for proc in self.procesosEnMemoria:
+                    proc.espera += 1
+                    proc.retorno += 1
 
     def MostrarMensaje(self):
 
